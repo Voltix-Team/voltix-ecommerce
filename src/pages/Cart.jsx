4ee423b92dbd036969e31c6f1a55f2484ed700ec
seif-alhorani <horanisaif5@gmail.com>
2026-04-08 12:40:40 +0300

    Add Cart Sample And Calculate the Total

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,36 +1,76 @@
 import { useState } from 'react';
-import  '../styles/style-cart.css'
-import logo from '../assets/Logo.png'
+import { ShoppingCart, User, Trash2, ShieldCheck, Truck, Minus, Plus } from 'lucide-react';
+
+const CartPage = () => {
+  const [CartItems, setCartItems] = useState([
+    {
+      id: 1,
+      category: 'AUDIO MASTERY',
+      name: 'Voltix Pro X1 Headphones',
+      description: 'Space Grey / Titanium Core',
+      price: 349.00,
+      quantity: 1,
+      image: 'https://placehold.co/100x100/1a1a1a/FFF?text=Headphones'
+    },
+    {
+      id: 2,
+      category: 'AUDIO MASTERY',
+      name: 'Voltix Pro X1 Headphones',
+      description: 'Space Grey / Titanium Core',
+      price: 349.00,
+      quantity: 1,
+      image: 'https://placehold.co/100x100/1a1a1a/FFF?text=Headphones'
+    },
+    {
+      id: 1,
+      category: 'AUDIO MASTERY',
+      name: 'Voltix Pro X1 Headphones',
+      description: 'Space Grey / Titanium Core',
+      price: 349.00,
+      quantity: 1,
+      image: 'https://placehold.co/100x100/1a1a1a/FFF?text=Headphones'
+    }
+
+  ]);
+  const discount = 0;
+  const subtotal = CartItems.reduce((sum,item)=>sum + item.price * item.quantity,0);
+  const total = subtotal - discount;
+  const handleQuantityChange = (id,value)=>{
+    setCartItems(CartItems.map(item=>{
+      if(item.id === id){
+        const newQuntity = item.quantity + value;
+        return {...item, quantity: newQuntity};
+      }
+    }))
+  }
+}
+
 export default function Counter() {
-  const [name, setName] = useState('Taylor');
-  const [age, setAge] = useState(42);
-
-  function handleNameChange(e) {
-    setName(e.target.value);
-  }
-
-  function handleAgeChange() {
-    setAge(age + 1);
-  }
 
   return (
-    <nav >
-        <ul>
-            <li><img src={logo} alt="logo" /></li>
-            <li>Audio</li>
-            <li>Computing</li>
-            <li>Mobile</li>
-            <li>Vision</li>
-            <li>Gaming</li>
-            <li>Deals</li>
-            <li>
-                <span>cart</span>
-            </li>
-            <li>
-                <span>profile-pic</span>
-            </li>
-        </ul>
-    </nav>
-    
+    <div className=''>
+      <nav>
+        <div>Voltix</div>
+        <div>
+          <a href="#">ِAudio</a>
+          <a href="#">Computing</a>
+          <a href="#">Mobile</a>
+          <a href="#">Vision</a>
+          <a href="#">Gaming</a>
+          <a href="#">Deals</a>
+        </div>
+        <div>
+          <div>
+            <ShoppingCart />
+            <span></span>
+          </div>
+          <User></User>
+        </div>
+      </nav>
+      <main>
+
+      </main>
+    </div>
+
   )
 }
