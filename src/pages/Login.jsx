3d559c96478ecfd17422454906cab5e54b5ca00c
Randa-Khaled <320210601121@gmail.com>
2026-04-08 16:33:51 +0300

    checkout page completed

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,7 +4,7 @@ import { auth } from "../firebase/firebaseConfig";
 import { signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import Button from "../components/UI/Button";
 import Input from "../components/UI/Input";
-import { Link } from "react-router-dom";
+
 import { HelpCircle } from "lucide-react";
 import logo from "../assets/Logo.png";
 
