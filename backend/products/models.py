b684d21e0ed376d275ac5e36d1cce51e0e0f38b5
seif-alhorani <horanisaif5@gmail.com>
2026-05-06 15:12:43 +0300

    create product model and views and serialize

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
 class Product(models.Model):
-    # Add just one field so it's valid
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(default=0)
+    category = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
--- backend/products/views.py
+++ backend/products/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Product
+from .serializers import ProductSerializer
 
-# Create your views here.
+class ProductViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
